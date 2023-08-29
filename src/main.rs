6105568b77decc96248569b481e6fbb425c7abec
SterlingBraiker <travisesteban91@gmail.com>
2023-08-29 19:19:12 -0400

    Updating Main

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,5 @@
-<<<<<<< HEAD
 use std::collections::HashMap;
-fn main() {
-	let mut scores = HashMap::new();
-	scores.insert(String::from("Blue"), 10);
-	scores.insert(String::from("Yellow"), 50);
-}
-=======
+
 /* --> directives */
 #![allow(non_snake_case)]
 #![allow(unused_imports)]
@@ -23,11 +17,3 @@ mod AuxFuncs;
 use crate::AuxFuncs::*;
 /* <--  imports */
 
-
-
-fn main() {
-    let aVec: Vec<Vec<usize>> = vec![vec![1, 2, 3], vec![1, 2, 3], vec![1, 2, 3]];
-    let aString: String = translateVecToCSV(&aVec);
-    println!("aString:\n{}", aString);
-}
->>>>>>> DaedriVictus/master
